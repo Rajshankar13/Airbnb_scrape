75d3992551a99ddbd3864b39c6888ec57100edb9
Rajshankar Khattar <rajshankarkhattar13@gmail.com>
2018-12-18 23:01:23 +0530

    Scrape through all pages in Airbnb

--- Airbnb/Airbnb/spiders/airbnb.py
+++ Airbnb/Airbnb/spiders/airbnb.py
@@ -16,6 +16,12 @@ class AirbnbSpider(scrapy.Spider):
         for restaurant in restaurants:
             yield scrapy.Request(url='https://www.airbnb.co.in/api/v2/similar_restaurants/{0}?key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=INR&locale=en-IN&number_of_guests=2&_format=default'.format(restaurant.get('id')), callback=self.parse)
 
+        pagination_metadata = data.get('explore_tabs')[0].get('pagination_metadata')
+        if(pagination_metadata.get('has_next_page')):
+            items_offset = pagination_metadata.get('items_offset')
+            section_offset = pagination_metadata.get('section_offset')
+            yield scrapy.Request(url='https://www.airbnb.co.in/api/v2/explore_tabs?version=1.3.9&satori_version=1.0.7&_format=for_explore_search_web&experiences_per_grid=20&items_per_grid=18&guidebooks_per_grid=20&auto_ib=false&fetch_filters=true&has_zero_guest_treatment=true&is_guided_search=true&is_new_cards_experiment=true&luxury_pre_launch=false&query_understanding_enabled=true&show_groupings=true&supports_for_you_v3=true&timezone_offset=330&client_session_id=99c29d30-9048-48ae-b5e3-2caa2cb6c45e&metadata_only=false&is_standard_search=true&refinement_paths%5B%5D=%2Frestaurants&selected_tab_id=restaurant_tab&place_id=ChIJCSF8lBZEwokRhngABHRcdoI&screen_size=medium&query=Brooklyn%2C%20NY%2C%20United%20States&_intents=p1&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=INR&locale=en-IN&items_offset={0}&section_offset={1}'.format(items_offset, section_offset), callback=self.parse_id)
+
     def parse(self, response):
         restaurant = json.loads(response.body).get('place_activity')
         yield  {
